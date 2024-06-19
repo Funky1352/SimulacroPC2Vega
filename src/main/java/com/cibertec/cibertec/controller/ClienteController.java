ed3987672e9e739f872471da220e4df45e22c4d7
Funky1352 <gerardo11235@hotmail.com>
2024-06-18 20:49:26 -0500

    Iniciando Repo

--- src/main/java/com/cibertec/cibertec/controller/ClienteController.java
+++ src/main/java/com/cibertec/cibertec/controller/ClienteController.java
@@ -4,7 +4,9 @@ import com.cibertec.cibertec.entity.Cliente;
 import com.cibertec.cibertec.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,8 +18,19 @@ public class ClienteController {
     @Autowired
     private ClienteService clienteService;
 
-    @GetMapping
+    @GetMapping("/listaTodos")
     public List<Cliente> getAllClientes() {
         return clienteService.listaCliente();
     }
+
+    @GetMapping("/buscarPorNombre")
+    public List<Cliente> getClienteByNombre(@RequestParam("nombres") String nombres) {
+        return clienteService.listaClientePorNombre(nombres + "%");
+    }
+
+    @GetMapping("/buscarPorDni/{dni}")
+    public List<Cliente> getClienteByDni(@PathVariable("dni") String dni){
+        return clienteService.listaClientePorDni(dni);
+    }
+
 }
